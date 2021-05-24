fd6459cd15dc83f8965bd512eedea82ae8efcf2c
DESKTOP-M7NLJHB\molag <molagowiec123@wp.pl>
2021-05-24 15:28:17 +0200

    drobne zmiany w mainie

--- main.cpp
+++ main.cpp
@@ -6,43 +6,43 @@ using namespace std;
 int main()
 {
 
-/*Container<int> c;
+Container<int> c;
 c.push_back(1);
 c.push_back(2);
 c.push_back(3);
 c.push(2,4);
 cout<<c<<endl;
+cout<<"Przenosze 2. element na koniec"<<endl;
 c.moveToEnd(2);
 cout<<c<<endl;
+cout<<"Zamieniam miejscami 1. i 3. element"<<endl;
 c.swap(1,3);
 cout<<c<<endl;
+cout<<"Usuwam 2. element"<<endl;
 c.pop(2);
 cout<<c<<endl;
+cout<<"Uzyskuje dostep do 2. elementu"<<endl;
+cout<<c[2]<<endl;
+cout<<"Zapis: "<<endl;
 c.save();
+cout<<"Odczyt: "<<endl;
 c.read();
 
 cout<<c<<endl;
 
-ComputerMonitor m1("comp1");
-ComputerMonitor m2("comp2");
-ComputerMonitor m3("comp3");
-Container<ComputerMonitor> d;
-d.push_back(m1);
-d.push_back(m2);
-d.push_back(m3);
-d.swap(2,3);
-cout<<d[2]<<endl;
-d.save();
-d.read();
-
-cout<<d<<endl;
 
 Container<string> s;
 s.push_back("keeke");
 s.push_back("eheheeh");
 cout<<s<<endl;
+cout<<"Dodaje na 1. pozycje"<<endl;
+s.push(1, "pierwszy");
+cout<<s<<endl;
+cout<<"Zapis"<<endl;
+s.save();
+cout<<"Odczyt"<<endl;
 s.read();
-cout<<s<<endl;*/
+cout<<s<<endl;
 
 interface();
     return 0;
